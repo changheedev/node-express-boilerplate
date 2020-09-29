b4485b3fc5ca4290a0d0a146b3468e1fb0dc4d20
Changehee Choi <changhee.dev@gmail.com>
2020-09-29 22:24:45 +0900

    feat: 모델 초기화 코드 구현
    - fs모듈을 이용하여 models 디렉토리에 구현된 모델 클래스 파일을 모두 읽어와 모듈로 import 하고 init 함수를 호출해주는 작업 구현
    
    - 개발 환경 또는 테스트 환경일 때 모델과 테이블을 sync 하도록 처리

--- src/main/database/init.js
+++ src/main/database/init.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
 const { Sequelize } = require('sequelize');
 const dbConfig = require('@config/db.config');
 
 const init = () => {
     const sequelize = new Sequelize({ ...dbConfig });
-    return { sequelize };
+    const modelsDirectory = 'src/main/models';
+
+    const models = Object.assign(
+        {},
+        ...fs.readdirSync(modelsDirectory).map((file) => {
+            const model = require('@models/' + file);
+            return {
+                [model.name]: model.init(sequelize),
+            };
+        }),
+    );
+
+    if (process.env.NODE_ENV !== 'production') {
+        const syncOption =
+            process.env.NODE_ENV === 'development' ? {} : { force: true };
+        sequelize.sync(syncOption);
+    }
+
+    return { models, sequelize };
 };
 
 module.exports = { init };
