44710ba86dc2921fe6ababdb463e7a1ecb3a17c3
Changehee Choi <changhee.dev@gmail.com>
2020-09-30 19:41:09 +0900

    refactor: JwtUtil 클래스 수정
    - 콜백을 사용하지 않는 sync 버전의 함수 사용으로 변경
    
    - verify 함수는 passport 사용으로 대체할 예정

--- src/main/utils/jwt.js
+++ src/main/utils/jwt.js
@@ -6,38 +6,24 @@ class JwtUtil {
     }
 
     generate(user) {
-        return new Promise((resolve, reject) => {
-            jwt.sign(
-                {
-                    id: user.id,
-                    username: user.name,
-                    email: user.email,
-                },
-                this.config.secret,
-                {
-                    expiresIn: this.config.expiresIn,
-                    issuer: this.config.issuer,
-                    subject: 'userInfo',
-                },
-                (err, token) => {
-                    if (err) reject(err);
-                    resolve(token);
-                },
-            );
-        });
+        return jwt.sign(
+            {
+                id: user.id,
+                username: user.name,
+                email: user.email,
+            },
+            this.config.secret,
+            {
+                expiresIn: this.config.expiresIn,
+                issuer: this.config.issuer,
+                subject: 'userInfo',
+            },
+        );
     }
 
     verify(token) {
-        return new Promise((resolve, reject) => {
-            jwt.verify(
-                token,
-                this.config.secret,
-                { issuer: this.config.issuer },
-                (err, decoded) => {
-                    if (err) reject(err);
-                    resolve(decoded);
-                },
-            );
+        return jwt.verify(token, this.config.secret, {
+            issuer: this.config.issuer,
         });
     }
 }
--- src/test/utils/jwt.specs.js
+++ src/test/utils/jwt.specs.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
-const JwtUtil = require('@/utils/jwt');
+const JwtUtil = require('@utils/jwt');
+
 const testJwtConfig = {
     secret: 'token-secret',
     expiresIn: '10h',
@@ -11,13 +12,13 @@ const testUser = {
     email: 'test@email.com',
 };
 
-describe('JwtProvider util class tests', () => {
+describe.only('JwtProvider util class tests', () => {
     describe('Generate token tests', () => {
-        it('토큰은 문자열 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"로 시작한다', async () => {
+        it('토큰은 문자열 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"로 시작한다', () => {
             //given
             const jwtUtil = new JwtUtil(testJwtConfig);
             //when
-            const token = await jwtUtil.generate(testUser);
+            const token = jwtUtil.generate(testUser);
             //then
             assert.equal(
                 token.startsWith('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'),
@@ -27,69 +28,53 @@ describe('JwtProvider util class tests', () => {
     });
 
     describe('Verify token tests', () => {
-        it('토큰 검증에 성공하면 토큰 정보가 반환된다', async () => {
+        it('토큰 검증에 성공하면 토큰 정보가 반환된다', () => {
             //given
             const jwtUtil = new JwtUtil(testJwtConfig);
-            const token = await jwtUtil.generate(testUser);
+            const token = jwtUtil.generate(testUser);
             //when
-            const claims = await jwtUtil.verify(token);
+            const claims = jwtUtil.verify(token);
             //then
             assert.equal(claims.id, testUser.id);
             assert.equal(claims.username, testUser.name);
             assert.equal(claims.email, testUser.email);
         });
 
-        it('secret이 동일하지 않으면 검증에 실패한다', async () => {
+        it('secret이 동일하지 않으면 검증에 실패한다', () => {
             //given
             const jwtUtil = new JwtUtil(testJwtConfig);
-            const token = await jwtUtil.generate(testUser);
+            const token = jwtUtil.generate(testUser);
 
-            //when
             const otherJwtConfig = { ...testJwtConfig };
             otherJwtConfig.secret = 'other-secret';
             const otherJwtUtil = new JwtUtil(otherJwtConfig);
-            return new Promise((resolve, reject) => {
-                otherJwtUtil
-                    .verify(token)
-                    //then
-                    .then(() => reject())
-                    .catch(() => resolve());
-            });
+
+            //when & then
+            assert.throws(() => otherJwtUtil.verify(token));
         });
 
-        it('issuer가 동일하지 않으면 검증에 실패한다', async () => {
+        it('issuer가 동일하지 않으면 검증에 실패한다', () => {
             //given
             const jwtUtil = new JwtUtil(testJwtConfig);
-            const token = await jwtUtil.generate(testUser);
+            const token = jwtUtil.generate(testUser);
 
-            //when
             const otherJwtConfig = { ...testJwtConfig };
             otherJwtConfig.issuer = 'other-issuer.com';
             const otherJwtUtil = new JwtUtil(otherJwtConfig);
-            return new Promise((resolve, reject) => {
-                otherJwtUtil
-                    .verify(token)
-                    //then
-                    .then(() => reject())
-                    .catch(() => resolve());
-            });
+
+            //when & then
+            assert.throws(() => otherJwtUtil.verify(token));
         });
 
-        it('토큰이 만료되면 검증에 실패한다', async () => {
+        it('토큰이 만료되면 검증에 실패한다', () => {
             //given
             const otherJwtConfig = { ...testJwtConfig };
             otherJwtConfig.expiresIn = '10'; //ms
             const jwtUtil = new JwtUtil(otherJwtConfig);
-            const token = await jwtUtil.generate(testUser);
+            const token = jwtUtil.generate(testUser);
 
-            //when
-            return new Promise((resolve, reject) => {
-                jwtUtil
-                    .verify(token)
-                    //then
-                    .then(() => reject())
-                    .catch(() => resolve());
-            });
+            //when & then
+            assert.throws(() => jwtUtil.verify(token));
         });
     });
 });
