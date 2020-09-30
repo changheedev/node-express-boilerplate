dd17e66bf7dafe991dc75983eef58f9490bd6ab5
Changehee Choi <changhee.dev@gmail.com>
2020-09-30 19:05:46 +0900

    feat: User 패스워드 암호화 처리
    - Sequelize hook 을 이용하여 db에 저장될 때 암호화 처리
    
    - Password 매칭 여부를 확인하는 함수 추가

--- src/main/models/user.js
+++ src/main/models/user.js
@@ -1,39 +1,50 @@
 const { DataTypes, Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
     static init(sequelize) {
-        return super.init(
-            {
-                id: {
-                    type: DataTypes.INTEGER.UNSIGNED,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true,
-                },
-                name: {
-                    type: new DataTypes.STRING(100),
-                    allowNull: false,
-                },
-                email: {
-                    type: new DataTypes.STRING(100),
-                    allowNull: false,
-                    unique: true,
-                },
-                password: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-            {
-                sequelize,
-                tableName: 'users',
-                engine: 'InnoDB',
-                charset: 'utf8',
-                indexes: [{ unique: true, fields: ['email'] }],
-                timestamps: false,
-            },
-        );
+        return super.init(scheme, { sequelize, ...initOptions });
+    }
+
+    verifyPassword(password) {
+        return bcrypt.compareSync(password, this.password);
     }
 }
+
+const scheme = {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: new DataTypes.STRING(100),
+        allowNull: false,
+    },
+    email: {
+        type: new DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+};
+
+const initOptions = {
+    tableName: 'users',
+    engine: 'InnoDB',
+    charset: 'utf8',
+    indexes: [{ unique: true, fields: ['email'] }],
+    timestamps: false,
+    hooks: {
+        beforeCreate: (user) => {
+            const salt = bcrypt.genSaltSync();
+            user.password = bcrypt.hashSync(user.password, salt);
+        },
+    },
+};
 
 module.exports = User;
