ecf463b5a2801e5b7d44c5b1aefd474b31fdfd8e
Changehee Choi <changhee.dev@gmail.com>
2020-10-11 16:13:10 +0900

    refactor: Sequelize 모델 초기화 코드 수정
    app을 초기화할때 loader 모듈에서 모델을 초기화를 진행한 다음 이후 서비스 모듈이나 라우터 모듈에서는 초기화 된 모델을 사용하려 했지만 loader 모듈이 실행되기 전에 다른 모듈의 require가 먼저 진행되면서 오류가 발생하는 문제가 있었다. require 시점을 조절해주는 방식으로 처리가 가능한 모듈도 있었지만 특정 모듈은 require 시점을 조절하기 어려웠고 이러한 문제로 models 모듈내에서 초기화를 진행하는 방법으로 수정.

--- src/main/loaders/sequelize.js
+++ src/main/loaders/sequelize.js
@@ -1,15 +1,44 @@
-const { initialize, sequelize } = require('@models');
+class SequelizeLoaderError extends Error {
+    constructor(msg) {
+        super(msg);
+    }
+}
 
 const sequelizeLoader = async () => {
-    initialize();
-    if (process.env.NODE_ENV !== 'production') {
-        await syncTables();
+    const sequelize = require('@models');
+    try {
+        await syncTables(sequelize);
+    } catch (err) {
+        await sequelize.close();
+        throw err;
     }
 };
 
-const syncTables = async () => {
-    const syncOption = process.env.NODE_ENV === 'test' ? { force: true } : {};
-    await sequelize.sync(syncOption);
+const syncTables = async (sequelize) => {
+    const transaction = await sequelize.transaction();
+    try {
+        if (process.env.NODE_ENV === 'test') {
+            await syncTablesInTestEnvironment(sequelize);
+        } else {
+            const syncOption =
+                process.env.NODE_ENV === 'development'
+                    ? { alter: { drop: false } }
+                    : {};
+            await sequelize.sync(syncOption);
+        }
+        await transaction.commit();
+    } catch (err) {
+        await transaction.rollback();
+        throw new SequelizeLoaderError(
+            `Table synchronization failed\n${err.message}`,
+        );
+    }
+};
+
+const syncTablesInTestEnvironment = async (sequelize) => {
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+    await sequelize.sync({ force: true });
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
 };
 
 module.exports = sequelizeLoader;
--- src/main/models/index.js
+++ src/main/models/index.js
@@ -2,10 +2,6 @@ const { Sequelize } = require('sequelize');
 const dbConfig = require('@config/db');
 const User = require('@models/user');
 
-const models = {
-    user: User,
-};
-
 const sequelize = new Sequelize({
     ...dbConfig,
     define: {
@@ -16,13 +12,21 @@ const sequelize = new Sequelize({
     },
 });
 
-const initialize = () => {
+const models = {
+    user: User,
+};
+
+const initModels = (models, sequelize) => {
     Object.values(models).forEach((model) => {
         model.init(sequelize);
     });
 };
 
-module.exports = {
-    initialize,
-    sequelize,
+const setAssociations = () => {
+    //연관관계 설정
 };
+
+initModels(models, sequelize);
+setAssociations();
+
+module.exports = sequelize;
